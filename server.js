566ee06b5703d9da47b1199b5b2f22e15f303689
Mohamed Mesahel <82197617+MohamedMesahel@users.noreply.github.com>
2021-09-20 11:33:44 -0700

    testing mongodb

--- server.js
+++ server.js
@@ -15,7 +15,7 @@ app.use(express.json());
 
 app.use(express.static("public"));
 
-mongoose.connect("mongodb://localhost/budget", {
+mongoose.connect("mongodb://localhost/budgetDB", {
   useNewUrlParser: true,
   useFindAndModify: false
 });
